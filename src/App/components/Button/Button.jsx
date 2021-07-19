b1f0eeb654235764931a8bedeb0d08d90dd2a26a
cmarti <cmarti@quechoisir.org>
2021-07-19 16:18:45 +0200

    usestate

--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -1,12 +1,40 @@
 import './Button.css';
+import PropTypes from 'prop-types';
+import {useState} from 'react';
 export const uneVar = 'Hello';
 
+/**
+ * Bouton personnalisable
+ * @param {object} props 
+ * @returns {element}
+ */
+
 function Button(props) {
+	const [isClicked, setisClicked] = useState(false);
 	return (
-		<button className="Button">
-			Mon bouton
+		<button style={{...props.style, backgroundColor:props.bgColor}} className={"Button"+(isClicked?' clicked':'')} 
+			onClick={(evt)=>{
+				setisClicked(true)
+				setTimeout(()=>{
+					setisClicked(false);
+
+				}, 300)
+				props.action('test 1')
+			}}>
+			{props.children}
 		</button>
 	);
+}
+
+
+Button.propTypes={
+  children:PropTypes.any.isRequired,
+  action:PropTypes.func.isRequired,
+  bgColor:PropTypes.string
+}
+
+Button.defaultProps={
+	bgColor: '#0033BB'
 }
 
 export default Button;
